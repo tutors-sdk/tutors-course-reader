3ca3cda50a37d2ac4e2c08371e314dcd2f60d0f2
Eamonn de Leastar <edeleastar@wit.ie>
2020-10-01 08:03:16 +0100

    load test course and list topics

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
-  export let name: string;
+  import { onMount } from "svelte";
+  import { Course } from "./services/course";
+
+  let course: Course = null;
+  onMount(async () => {
+    const url = "https://wit-hdip-comp-sci-2020-web-development.netlify.app/tutors.json";
+    await fetch(url)
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+        course = new Course(url);
+        course.init(data, url);
+      });
+  });
 </script>
 
-<main>
-  <h1>Hello {name}</h1>
-</main>
+{#if course}
+  <main>
+    <h3>Course Title: {course.lo.title}</h3>
+    <ul>
+      {#each course.lo.los as lo}
+        <li>{lo.title}</li>
+      {/each}
+    </ul>
+  </main>
+{/if}
