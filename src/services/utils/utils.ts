e25289edb13b0d0fa676c62aafccf2488a75cbc3
Eamonn de Leastar <edeleastar@wit.ie>
2020-11-11 07:52:55 +0000

    format date utility

--- src/services/utils/utils.ts
+++ src/services/utils/utils.ts
@@ -129,3 +129,15 @@ export function decrypt(str: string): string {
   const value = raw.toString(Crypto.enc.Utf8);
   return value;
 }
+
+export function formatDate(date): string {
+  var d = new Date(date),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [year, month, day].join("-");
+}
