0973ca684103f83285e9cca81eb3c5bb2d8a031e
Eamonn de Leastar <edeleastar@wit.ie>
2020-11-14 17:07:05 +0000

    reduce content of calendar button

--- src/components/iconography/TutorsIcon.svelte
+++ src/components/iconography/TutorsIcon.svelte
@@ -4,7 +4,7 @@
   export let version = "2.2.0";
 </script>
 
-<div class="uk-card uk-card-small uk-card-hover uk-text-center uk-padding-remove">
+<div class="uk-card uk-card-small uk-card-hover uk-text-center uk-padding-remove" uk-tooltip="title: Tutors Reader application source; pos: bottom">
   <a target="”_blank”" href="https://github.com/tutors-sdk/tutors-svelte">
     <div>
       <Icon class="icon-moduleHome" data={getIconFromType('tutors')} scale="2" />
