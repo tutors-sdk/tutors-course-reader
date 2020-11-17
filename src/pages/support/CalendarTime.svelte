3b3c35fcd9427ff6775a3a2b34fe77f4114a03e6
Eamonn de Leastar <edeleastar@wit.ie>
2020-11-17 10:25:17 +0000

    import correct options for calendar sheet

--- src/pages/support/CalendarTime.svelte
+++ src/pages/support/CalendarTime.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { onMount, getContext } from "svelte";
   import { Grid } from "ag-grid-community";
-  import { options } from "../../components/sheets/lab-sheet";
+  import { options } from "../../components/sheets/calendar-sheet";
   import { CalendarSheet } from "../../components/sheets/calendar-sheet";
   import { Cache } from "../../services/course/cache";
   import { MetricsService } from "../../services/analytics/metrics-service";
