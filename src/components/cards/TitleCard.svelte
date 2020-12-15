0998c2424b0b1d3690aeaf74e6404c29fdf0a4a8
Cathal O’Riordan <cathal@madein75.com>
2020-12-15 14:59:20 +0000

    add breakpoints to calendar, tutors icon, week and toc button

--- src/components/cards/TitleCard.svelte
+++ src/components/cards/TitleCard.svelte
@@ -3,6 +3,8 @@
   import { navigatorProps, week } from "../../services/course/stores";
   import { getContext } from 'svelte';
   import CalendarCard from "./CalendarCard.svelte"
+  import { revealSidebar, courseUrl } from "../../services/course/stores";
+  import TocBtn from "../navigators/TocBtn.svelte";
 
   const { open } = getContext('simple-modal');
   const showCalendar = () => {
@@ -10,21 +12,28 @@
   };
 </script>
 
-<div class="bg-white border shadow justify-center items-center hidden lg:flex w-1/2 p-4 ml-4 mt-4">
-    <div class="w-1/6"><img src={$navigatorProps.title.img} width="60" alt="img" /></div>
+<div class="bg-white border shadow justify-center items-center flex w-1/2 p-4 ml-4 mt-4">
+  {#if $navigatorProps.tocShow}
+    <div class="w-1/6 hidden md:block">
+      <button on:click={() => revealSidebar.set(true)}>
+        <TocBtn />
+      </button>
+    </div>
+  {/if}  
+    <div class="hidden md:block w-1/6"><img src={$navigatorProps.title.img} width="60" alt="img" /></div>
     <div class="w-2/3">
       <div class="course-title">{$navigatorProps.title.title}</div>
       <div class="uk-text-muted uk-text-small">{$navigatorProps.title.subTitle}</div>
     </div>
-    {#if $week}
-      <div class="w=1/5">
+  {#if $week}
+    <div class="hidden lg:block w=1/5">
         <button on:click={showCalendar} class="py-2 px-4 border" uk-tooltip="title: Click for module calendar; pos: bottom">
           <div class="uk-text-bold">  {$week.title} </div>
           <div class="uk-text-small uk-text-light"> calendar </div>
         </button>
       </div>
-    {/if}
-    <div class="py-2 px-4 w-1/6">
+  {/if}
+    <div class="hidden lg:block py-2 px-4 w-1/6">
       <TutorsIcon />
     </div>
 </div>
