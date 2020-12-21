473b8bedf3a98a002fe61500720ba1060dc8de48
Eamonn de Leastar <edeleastar@wit.ie>
2020-12-21 17:33:31 +0000

    remove items

--- src/environment.ts
+++ src/environment.ts
@@ -1,16 +1,16 @@
 let keys = {
   auth0: {
-    clientId: "GaRinBEbq5RiS2hwsKe4Tr96vQ4gmHAv",
-    domain: "dev-ag-6r7t5.eu.auth0.com",
-    redirectUri: "http://localhost:5000",
+    clientId: "XXX",
+    domain: "XXX",
+    redirectUri: "XXX",
   },
   firebase: {
-    apiKey: "AIzaSyAzoLn7SfXkH58oBRq4dFjLW6DgIpdUf9w",
-    databaseURL: "https://tutors-177ed.firebaseio.com",
-    projectId: "tutors-177ed",
+    apiKey: "XXX",
+    databaseURL: "XXX",
+    projectId: "XXX",
   },
-  ga: "UA-147419187-4",
-  theoplayer: "24abfd34-e8c7-48cf-9043-f1b3916b241d",
+  ga: "XXX",
+  theoplayer: "XXX",
 };
 
 export function getKeys() {
