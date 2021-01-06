44c358ead07929dbfccb4877d44ad8360d7e5307
Eamonn de Leastar <edeleastar@wit.ie>
2021-01-06 07:41:16 +0000

    introduce change log

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Tutors Changelog
+
+## [3.0.1] - 2021-01-06
+
+- reset scroll position on each lab step change
+- border around tooltip
+
+## [3.0.0] - 2021-01-04
+
+- First port of new version - Replace UIKit with Tailwind
--- src/components/iconography/TutorsIcon.svelte
+++ src/components/iconography/TutorsIcon.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import Icon from "./Icon.svelte";
 
-  export let version = "3.0.0";
+  export let version = "3.0.1";
 </script>
 
 <Icon type="tutors" scale="2" target="_blank" link="https://github.com/tutors-sdk/tutors-svelte" toolTip="Tutors Reader {version}" />
