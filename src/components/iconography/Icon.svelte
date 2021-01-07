b867af8fe5c04cab01e0365b2b4ee59f32345237
Eamonn de Leastar <edeleastar@wit.ie>
2021-01-07 11:26:59 +0000

    enable non-hyperlinked icons

--- src/components/iconography/Icon.svelte
+++ src/components/iconography/Icon.svelte
@@ -3,19 +3,28 @@
   import Icon from "svelte-awesome";
 
   export let type;
-  export let toolTip;
-  export let link;
-  export let target;
+  export let toolTip = "";
+  export let link = "";
+  export let target = "";
   export let scale;
 
 </script>
 {#if getIcon(type)}
-  <a class="tooltip hover:border-gray-900" target="{target}" href="{link}">
-    {#if toolTip}
-      <span class="tooltip-text">{toolTip}</span>
-    {/if}
-    <Icon data={getIcon(type).icon} scale="{scale}" style="color:{getIcon(type).colour};" />
-  </a>
+  {#if link}
+    <a class="tooltip hover:border-gray-900" target="{target}" href="{link}">
+      {#if toolTip}
+        <span class="tooltip-text">{toolTip}</span>
+      {/if}
+      <Icon data={getIcon(type).icon} scale="{scale}" style="color:{getIcon(type).colour};" />
+    </a>
+  {:else}
+    <span class="tooltip">
+      {#if toolTip}
+        <span class="tooltip-text">{toolTip}</span>
+      {/if}
+      <Icon data={getIcon(type).icon} scale="{scale}" style="color:{getIcon(type).colour};" />
+    </span>
+  {/if}
 {:else}
   <h1> {type} </h1>
 {/if}
