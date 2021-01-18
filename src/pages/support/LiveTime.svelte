f2266eee71a2db7195cc80fd3a90123cabf51bdb
Eamonn de Leastar <edeleastar@wit.ie>
2021-01-18 16:00:26 +0000

    changelog + version update

--- src/pages/support/LiveTime.svelte
+++ src/pages/support/LiveTime.svelte
@@ -5,7 +5,8 @@
   import type { User } from "../../services/analytics/metrics-types";
   import { Grid } from "ag-grid-community";
   import { Cache } from "../../services/course/cache";
-  import { studentsOnline} from "../../services/course/stores"
+  import { studentsOnline } from "../../services/course/stores";
+  import { RingLoader } from "svelte-loading-spinners";
 
   let canUpdate = false;
   const func = () => {
@@ -65,10 +66,18 @@
 
 <div class="flex justify-around justify-center p-1">
   <div class="w-1/2">
-    <div class="text-base font-light text-gray-900"> Students online now: {$studentsOnline} </div>
+    <div class="text-base font-light text-gray-900">
+      {#if canUpdate}
+        Students online now: {$studentsOnline}
+      {:else}
+        <div class="flex flex-col justify-center items-center">
+          <span class="text-base font-light text-gray-900"> Loading ... one moment please.</span>
+          <RingLoader size="30" color="#FF3E00" unit="px" />
+        </div>
+      {/if}
+    </div>
   </div>
 </div>
 <div style="height:{liveHeight}px">
   <div bind:this={live} style="height: 100%; width:100%" class="ag-theme-balham" />
 </div>
-
