bcd80fc8b25c8c294cec431d8dce39fbca22cacd
Eamonn de Leastar <edeleastar@wit.ie>
2021-01-18 15:57:58 +0000

    loading indicator

--- src/environment.ts
+++ src/environment.ts
@@ -1,16 +1,16 @@
 let keys = {
   auth0: {
-    clientId: "XXX",
-    domain: "XXX",
-    redirectUri: "XXX",
+    clientId: "GaRinBEbq5RiS2hwsKe4Tr96vQ4gmHAv",
+    domain: "dev-ag-6r7t5.eu.auth0.com",
+    redirectUri: "http://localhost:5000",
   },
   firebase: {
-    apiKey: "XXX",
-    databaseURL: "XXX",
-    projectId: "XXX",
+    apiKey: "AIzaSyAzoLn7SfXkH58oBRq4dFjLW6DgIpdUf9w",
+    databaseURL: "https://tutors-177ed.firebaseio.com",
+    projectId: "tutors-177ed",
   },
-  ga: "XXX",
-  theoplayer: "XXX",
+  ga: "UA-147419187-4",
+  theoplayer: "24abfd34-e8c7-48cf-9043-f1b3916b241d",
 };
 
 export function getKeys() {
