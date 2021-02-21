ce12e47ba190bd1f4d2f3057fdf5d31b6dc11ccf
Eamonn de Leastar <edeleastar@wit.ie>
2021-02-21 17:28:23 +0000

    student card in live view to include images of topic + lab

--- src/components/cards/StudentCard.svelte
+++ src/components/cards/StudentCard.svelte
@@ -7,8 +7,8 @@
 
 <style>
   .card {
-    max-width: 150px;
-    min-width: 150px;
+    max-width: 200px;
+    min-width: 200px;
     height: auto
   }
 </style>
@@ -16,20 +16,35 @@
 <div
   class="font-light font-sm card m-4 block bg-white border shadow-md hover:shadow-xl dark:hover:bg-white dark:hover:text-black rounded-lg overflow-hidden dark:bg-black dark:text-white"
   in:fade={{ duration: 800 }}>
-  <div class="text-center"> {student.name} </div>
+  <div class="text-center p-1"> {student.name} </div>
   <hr>
-  <div class="flex justify-center">
-    <img loading="lazy" class="object-scale-down p-1 h-24" src="{student.img}" alt="{student.nickname}">
-  </div>
-  <hr>
-  <div class="p-2 text-center text-xs">
-    <div class="italic p-2"> {new Date(student.time).toLocaleTimeString()} </div>
-    <hr>
-    {#if student.topic }
-      <div><span class="italic"> Topic:&nbsp</span>{student.topic.lo.title}</div>
-    {/if}
-    {#if student.lab }
-      <div><span class="italic"> Lab:&nbsp</span>{student.lab.title}</div>
-    {/if}
+  <div class="grid grid-cols-2 gap-4">
+    <div>
+      <img loading="lazy" class="object-scale-down  h-24" src="{student.img}" alt="{student.nickname}">
+    </div>
+    <div>
+      <img loading="lazy" class="object-scale-down  h-24" src="{student.topic.lo.img}" alt="{student.nickname}">
+    </div>
+    <div>
+      {#if student.lab }
+        <img loading="lazy" class="object-scale-down  h-24" src="{student.lab.img}" alt="{student.nickname}">
+      {/if}
+    </div>
+    <div class="text-xs text-center">
+      <div class="mb-2">
+        {#if student.topic }
+          <div><span class="italic"> </span>{student.topic.lo.title}</div>
+        {/if}
+        {#if student.lab }
+          <div><span class="italic"> </span>{student.lab.title}</div>
+        {/if}
+      </div>
+      <hr>
+      <div class="italic">
+        {new Date(student.time).toLocaleTimeString()}
+      </div>
+    </div>
   </div>
 </div>
+
+<!--  <hr>--><!--  <div class="flex justify-center">--><!--    <img loading="lazy" class="object-scale-down p-1 h-24" src="{student.img}" alt="{student.nickname}">--><!--  </div>--><!--  <hr>--><!--  <div class="p-2 text-center text-xs">--><!--      <div class="italic p-2"> {new Date(student.time).toLocaleTimeString()} </div>--><!--      <hr>--><!--      {#if student.topic }--><!--        <div><span class="italic"> Topic:&nbsp</span>{student.topic.lo.title}</div>--><!--      {/if}--><!--      {#if student.lab }--><!--        <div><span class="italic"> Lab:&nbsp</span>{student.lab.title}</div>--><!--      {/if}--><!--    </div>--><!--</div>-->
