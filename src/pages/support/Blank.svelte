3a15a04c0310d11fb8eac147afbcdef4b1b46030
Eamonn de Leastar <edeleastar@wit.ie>
2021-03-12 08:40:30 +0000

    display tutors icon on blank page

--- src/pages/support/Blank.svelte
+++ src/pages/support/Blank.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+  import Icon from "../../components/iconography/Icon.svelte";
+</script>
+
+<div class="bg-white text-center m-4 p-4">
+  <Icon type="tutors" scale="20" />
+
+</div>
 <div class="container mx-auto text-center mt-8">
-  <div>Tutors course reader</div>
+  <div>Tutors   course reader</div>
 </div>
