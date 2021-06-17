6e4444d1f61418e722316e1e297340ed2ccc5b5f
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2021-06-17 22:09:53 +0100

    set font

--- src/TailwindCSS.svelte
+++ src/TailwindCSS.svelte
@@ -7,6 +7,7 @@
   @tailwind utilities;
 
   @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+
   .tooltip .tooltip-text {
     @apply invisible p-1 absolute z-50 inline-block mt-12 text-sm rounded-lg border border-gray-900 bg-white text-gray-900;
   }
