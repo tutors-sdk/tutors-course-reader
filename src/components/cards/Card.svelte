a082040a387a2aef6dd2d1fdee55b0fab2de5a8e
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2021-06-24 09:48:29 +0100

    updated card design to reflect older style

--- src/components/cards/Card.svelte
+++ src/components/cards/Card.svelte
@@ -25,24 +25,25 @@
   }
 </style>
 
-<div class="card text-center shadow-2xl bg-base-300 w-full xl:w-1/6 lg:w-1/4 md:w-1/3 m-2">
-<a href="{lo.route}" target="{target}" in:fade={{ duration: 800 }}>
-  <figure class="px-10 pt-10">
-    <img loading="lazy" class="rounded-xl" src="{lo.img}" alt="{lo.title}">
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">{lo.title}</h2>
-    <Icon type="{lo.type}" />
-  <div class="px-2 py-4 text-center text-base-content">
-    {#if $currentCourse && !$currentCourse.areVideosHidden()}
-      {#if lo.video && lo.type !== "video"}
-        <Icon link="{lo.video}" type="video" toolTip="Play video for this talk"/>
-      {/if}
-    {/if}
-    <p>
-      {@html lo.summary}
-    </p>
-  </div>
-  </div>
-</a>
+<div class="card text-center shadow-2xl bg-base-300 w-full xl:w-1/5 lg:w-1/4 md:w-1/3 m-2">
+  <a href="{lo.route}" target="{target}" in:fade={{ duration: 800 }}>
+    <div class="flex flex-row justify-between items-center p-2">
+      <h3 class="card-title pt-3">{lo.title}</h3>
+      <Icon type="{lo.type}" />
+    </div>
+    <figure class="px-1 pt-1">
+      <img loading="lazy" class="rounded-xl" src="{lo.img}" alt="{lo.title}">
+    </figure>
+    <div class="card-body">
+      <div class="px-1 py-1 text-center text-base-content line-clamp-3">
+        {#if $currentCourse && !$currentCourse.areVideosHidden()}
+          {#if lo.video && lo.type !== "video"}
+            <Icon link="{lo.video}" type="video" toolTip="Play video for this talk" />
+          {/if}
+        {/if}<p>
+        {@html lo.summary}
+      </p>
+      </div>
+    </div>
+  </a>
 </div>
