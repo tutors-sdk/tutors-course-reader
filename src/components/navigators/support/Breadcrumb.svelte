09d02f6b2ff20af2e25a441e2b1bd6084bfd5f08
Eamonn de Leastar <edeleastar@wit.ie>
2021-06-26 07:12:23 +0100

    reworked breadcrumbs

---
+++ src/components/navigators/support/Breadcrumb.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Icon from "../../iconography/Icon.svelte";
+  import type { Lo } from "../../../services/course/lo";
+
+  export let lo:Lo;
+</script>
+
+<li>
+  <Icon type="{lo.type}" />
+  <a href="{lo.route}"> &nbsp {lo.title}</a>
+</li>
