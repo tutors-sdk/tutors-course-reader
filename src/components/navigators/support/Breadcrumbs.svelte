ca07617ba3647084ac2dfd6ff6cca160fa1768e2
Eamonn de Leastar <edeleastar@wit.ie>
2021-06-26 08:03:30 +0100

    include parent programme in breadcrumb

--- src/components/navigators/support/Breadcrumbs.svelte
+++ src/components/navigators/support/Breadcrumbs.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
-  import { navigatorProps } from "../../../services/course/stores";
+  import { navigatorProps, currentCourse} from "../../../services/course/stores";
   import Breadcrumb from "./Breadcrumb.svelte";
+  import Icon from "../../iconography/Icon.svelte";
 </script>
 
 <div class="text-s breadcrumbs lg:flex-auto text-neutral-content p-3">
   <ul>
+    {#if $currentCourse.lo.properties?.parent != null}
+      <li>
+        <Icon type="moduleHome" />
+        <a href="#/{$currentCourse.lo.properties?.parent}"> &nbsp Programme Home </a>
+      </li>
+    {/if}
     {#if $navigatorProps.lo.parentLo}
       {#if $navigatorProps.lo.parentLo.parentLo}
         {#if $navigatorProps.lo.parentLo.parentLo.parentLo}
-          <Breadcrumb lo={$navigatorProps.lo.parentLo.parentLo.parentLo}/>
+          <Breadcrumb lo={$navigatorProps.lo.parentLo.parentLo.parentLo} />
         {/if}
-        <Breadcrumb lo={$navigatorProps.lo.parentLo.parentLo}/>
+        <Breadcrumb lo={$navigatorProps.lo.parentLo.parentLo} />
       {/if}
-      <Breadcrumb lo={$navigatorProps.lo.parentLo}/>
+      <Breadcrumb lo={$navigatorProps.lo.parentLo} />
     {/if}
-    <Breadcrumb lo={$navigatorProps.lo}/>
+    <Breadcrumb lo={$navigatorProps.lo} />
   </ul>
 </div>
