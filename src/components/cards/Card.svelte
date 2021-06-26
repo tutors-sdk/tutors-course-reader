17a2857aee0489ec96cfd6b6924736570429ade9
Eamonn de Leastar <edeleastar@wit.ie>
2021-06-26 06:52:30 +0100

    more compact card style

--- src/components/cards/Card.svelte
+++ src/components/cards/Card.svelte
@@ -25,17 +25,17 @@
   }
 </style>
 
-<div class="card text-center shadow-2xl bg-base-300 w-full xl:w-1/5 lg:w-1/4 md:w-1/3 m-2">
+<div class="card compact text-center shadow-2xl bg-base-100 w-full 2xl:w-1/6 xl:w-1/5 lg:w-1/4 md:w-1/3 m-2">
   <a href="{lo.route}" target="{target}" in:fade={{ duration: 800 }}>
     <div class="flex flex-row justify-between items-center p-2">
-      <h3 class="card-title pt-3">{lo.title}</h3>
+      <h3 class="card-title pt-1">{lo.title}</h3>
       <Icon type="{lo.type}" />
     </div>
-    <figure class="px-1 pt-1">
-      <img loading="lazy" class="rounded-xl" src="{lo.img}" alt="{lo.title}">
+    <figure class="flex justify-center">
+      <img loading="lazy" class="rounded-xl object-scale-down p-1 h-48" src="{lo.img}" alt="{lo.title}">
     </figure>
     <div class="card-body">
-      <div class="px-1 py-1 text-center text-base-content line-clamp-3">
+      <div class="text-center text-base-content line-clamp-3">
         {#if $currentCourse && !$currentCourse.areVideosHidden()}
           {#if lo.video && lo.type !== "video"}
             <Icon link="{lo.video}" type="video" toolTip="Play video for this talk" />
