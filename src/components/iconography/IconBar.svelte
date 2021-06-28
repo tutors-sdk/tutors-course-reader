0d39a1941ca71168a269ae6add701463d86bb26c
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2021-06-28 11:59:28 +0100

    list changed to span

--- src/components/iconography/IconBar.svelte
+++ src/components/iconography/IconBar.svelte
@@ -7,8 +7,10 @@
 
 <div class="flex-wrap lg:flex-none shadow-lg bg-neutral text-neutral-content rounded-box horizontal ml-2 mt-2 lg:mt-0">
   {#each nav.bar as i}
-    <li class="btn btn-square btn-ghost">
-      <Icon toolTip="{i.tip}" type="{i.icon}" link="{i.link}" target="{i.target}"/>
-    </li>
+    <span>
+      <a href="{i.link}" target="{i.target}">
+      <Icon toolTip="{i.tip}" type="{i.icon}" button="true"/>
+      </a>
+    </span>
   {/each}
 </div>
