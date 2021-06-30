697c005cb949e6eee03d8dab37fbcd3245732591
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2021-06-30 09:50:55 +0100

    thinner card font

--- src/components/cards/Card.svelte
+++ src/components/cards/Card.svelte
@@ -28,7 +28,7 @@
 <div class="card compact text-center shadow-2xl bg-base-100 w-full 2xl:w-1/6 xl:w-1/5 lg:w-1/4 md:w-1/3 m-2">
   <a href="{lo.route}" target="{target}" in:fade={{ duration: 800 }}>
     <div class="flex flex-row justify-between items-center p-3">
-      <h3 class="card-title">{lo.title}</h3>
+      <h3 class="card-title font-normal">{lo.title}</h3>
       <Icon type="{lo.type}" />
     </div>
     <figure class="flex justify-center">
