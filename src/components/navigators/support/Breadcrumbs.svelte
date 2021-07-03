4fccab0bb0827ca1fb6caa3e15a162851d217d04
Eamonn de Leastar <edeleastar@wit.ie>
2021-07-03 09:19:45 +0100

    use recursive function to retrieve lo chain

--- src/components/navigators/support/Breadcrumbs.svelte
+++ src/components/navigators/support/Breadcrumbs.svelte
@@ -2,27 +2,29 @@
   import { currentCourse, currentLo } from "../../../services/course/stores";
   import Breadcrumb from "./Breadcrumb.svelte";
   import Icon from "../../iconography/Icon.svelte";
+  import type { Lo } from "../../../services/course/lo";
+  
+  function crumbs(lo: Lo, los: Lo[]) {
+    if (lo) {
+      crumbs(lo.parentLo, los);
+      los.push(lo);
+    }
+    return los;
+  }
 </script>
 
-<div class="shadow-lg bg-neutral rounded-box horizontal text-neutral-content text-s breadcrumbs flex-auto p-2 overflow-x-hidden">
+<div
+  class="shadow-lg bg-neutral rounded-box horizontal text-neutral-content text-s breadcrumbs flex-auto p-2 overflow-x-hidden">
   <ul>
     {#if $currentCourse.lo.properties?.parent != null}
       <li>
         <a class="btn btn-sm" href="#/{$currentCourse.lo.properties?.parent}">
-        <Icon type="moduleHome" /></a>
+          <Icon type="moduleHome" />
+        </a>
       </li>
     {/if}
-    {#if $currentLo}
-      {#if $currentLo.parentLo}
-        {#if $currentLo.parentLo.parentLo}
-          {#if $currentLo.parentLo.parentLo.parentLo}
-            <Breadcrumb lo={$currentLo.parentLo.parentLo.parentLo} />
-          {/if}
-          <Breadcrumb lo={$currentLo.parentLo.parentLo} />
-        {/if}
-        <Breadcrumb lo={$currentLo.parentLo} />
-      {/if}
-      <Breadcrumb lo={$currentLo} />
-    {/if}
+    {#each crumbs($currentLo, []) as lo}
+      <Breadcrumb {lo} />
+    {/each}
   </ul>
 </div>
