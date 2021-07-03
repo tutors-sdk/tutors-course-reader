e5e84111f22421dcef2c7ab502f3061f5d3f6cfb
Eamonn de Leastar <edeleastar@wit.ie>
2021-07-03 09:20:00 +0100

    subtitle more intelligent

--- src/components/navigators/support/TitleCard.svelte
+++ src/components/navigators/support/TitleCard.svelte
@@ -13,16 +13,21 @@
   {/if}
   <div class="flex-1 hidden px-2 mx-2 lg:block">
     <p class="text-lg font-bold">{$currentLo.title}</p>
-    <p class="text-sm font-bold">{$currentCourse.lo.title}</p>
+    {#if $currentLo.title != $currentCourse.lo.title}
+      <p class="text-sm font-bold">{$currentCourse.lo.title}</p>
+    {:else}
+      <p class="text-sm font-bold">{$currentCourse.lo.properties.credits}</p>
+    {/if}
   </div>
 {/if}
 <div class="justify-center flex-1 hidden px-2 mx-2 lg:flex text-left text-base-content">
   {#if $currentCourse.currentWeek}
-    <div data-tip="Module Calendar" class="hidden lg:block w-auto h-auto pl-4 pr-4 border rounded-lg bg-base-100 text-base-content tooltip">
+    <div data-tip="Module Calendar"
+         class="hidden lg:block w-auto h-auto pl-4 pr-4 border rounded-lg bg-base-100 text-base-content tooltip">
       <label for="title-modal" class="modal-button">
-      <div class="text-l pt-2">{$currentCourse.currentWeek.title} </div>
-      <div class="text-sm"> calendar</div>
-    </label> <input type="checkbox" id="title-modal" class="modal-toggle">
+        <div class="text-l pt-2">{$currentCourse.currentWeek.title} </div>
+        <div class="text-sm"> calendar</div>
+      </label> <input type="checkbox" id="title-modal" class="modal-toggle">
       <CalendarCard />
     </div>
   {/if}
