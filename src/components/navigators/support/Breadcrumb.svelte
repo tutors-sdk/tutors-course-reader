a0a3693eb34b2e81816148a4c519c72f3737862c
Eamonn de Leastar <edeleastar@wit.ie>
2021-07-14 10:36:08 +0100

    negative margins + truncate text in breadcrumbs

--- src/components/navigators/support/Breadcrumb.svelte
+++ src/components/navigators/support/Breadcrumb.svelte
@@ -7,11 +7,18 @@
   if (lo.type === "unit") {
     unitId = lo.id;
   }
+
+  function truncate(input) {
+    if (input.length > 26) {
+      return input.substring(0, 26) + '...';
+    }
+    return input;
+  };
 </script>
 
 <li>
-  <a class="btn btn-sm capitalize" href="{lo.route}{unitId}">
+  <a class="btn btn-sm capitalize -ml-4" href="{lo.route}{unitId}">
     <Icon type="{lo.type}" />
-    <span class="hidden lg:block lg:pl-2">{lo.title}</span></a>
+    <span class="hidden lg:block lg:pl-2 -ml-1 -mr-4">{truncate(lo.title)}</span></a>
 </li>
 
