f357853ce64c05d61e27aaa45f050ba6de3a35ef
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2021-09-05 18:28:03 +0100

    changed visual theme in header to button

--- src/components/navigators/support/DarkMode.svelte
+++ src/components/navigators/support/DarkMode.svelte
@@ -27,10 +27,8 @@
 
 </script>
 <div class="flex-none tooltip tooltip-top dropdown dropdown-end dropdown-hover">
-  <button class="px-3">
-    <Icon type="dark" toolTip="Change Visual Theme" />
-  </button>
-  <ul class="shadow menu dropdown-content bg-base-100 text-base-content w-52 p-1">
+    <Icon type="dark" toolTip="Change Visual Theme" button="true"/>
+  <ul class="shadow menu dropdown-content bg-base-100 text-base-content w-52 p-1 rounded-lg">
     {#each themes as theme}
       <button on:click={() => setTheme(theme)} class="bg-base-100 text-base-content py-2" data-theme="{theme}">{theme}</button>
     {/each}
