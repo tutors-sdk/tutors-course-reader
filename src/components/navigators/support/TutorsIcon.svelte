6e3212ef6cf87cc792c67bfc5dbea4cd2c58dd65
Jordan Harrison <contact@jordanharrison.ie>
2021-11-04 21:24:22 +0000

    link to tutors.dev

--- src/components/navigators/support/TutorsIcon.svelte
+++ src/components/navigators/support/TutorsIcon.svelte
@@ -3,7 +3,7 @@
   export let version = "4.3.3";
 </script>
 <div class="tooltip" data-tip="Tutors Reader {version}">
-  <a target="_blank" href="https://tutors-docs.netlify.app">
+  <a target="_blank" href="https://tutors.dev">
     <div class="btn btn-square btn-ghost rounded-2xl">
       <!-- Generator: Adobe Illustrator 22.0.1, SVG Export Plug-In  -->
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
