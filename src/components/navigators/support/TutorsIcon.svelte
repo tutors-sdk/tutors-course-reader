e1f02dbff398477577212b94e82adb51d09383f4
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2021-11-09 13:14:50 +0000

    Update TutorsIcon.svelte

--- src/components/navigators/support/TutorsIcon.svelte
+++ src/components/navigators/support/TutorsIcon.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import Icon from "../../iconography/Icon.svelte";
-  export let version = "4.3.3";
+  export let version = "4.4.0";
 </script>
 <div class="tooltip" data-tip="Tutors Reader {version}">
   <a target="_blank" href="https://tutors.dev">
