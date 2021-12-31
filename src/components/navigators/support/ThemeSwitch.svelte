024d8c71f2c4fe2e8622c105e27995012cf82316
Eamonn de Leastar <edeleastar@wit.ie>
2021-12-31 12:53:05 +0000

    better tooltip defaut locations

--- src/components/navigators/support/ThemeSwitch.svelte
+++ src/components/navigators/support/ThemeSwitch.svelte
@@ -15,7 +15,7 @@
 
 
 <div class="flex-none tooltip tooltip-top dropdown dropdown-end dropdown-hover">
-  <Icon type="dark" toolTip="Change Visual Theme" button="true" />
+  <Icon type="dark" button="true" />
   <ul class="shadow menu dropdown-content bg-base-100 text-base-content w-48 p-1 rounded-xl">
     {#each themes as theme}
       <button on:click={() => setTheme(theme)} class="bg-base-100 text-base-content py-2"
