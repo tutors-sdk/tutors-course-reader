4131b5fd8ac90514568a97815559ec3f3f78a785
Eamonn de Leastar <edeleastar@wit.ie>
2022-02-20 07:51:51 +0000

    note card introduced

---
+++ src/components/cards/NoteCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { Lo } from "../../reader-lib/types/lo-types";
+  import { convertMd } from "../../reader-lib/utils/markdown-utils";
+
+  export let lo: Lo;
+  let contentHtml = "";
+  const url = lo.route.replace("/#/note/", "");
+  contentHtml = convertMd(lo.contentMd, url);
+</script>
+
+<article class="notecontent" >
+  {@html contentHtml}
+</article>
+
+<style>
+  :global(.notecontent pre) {
+    color : white;
+  }
+</style>
